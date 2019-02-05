f3b1ee4a0c4ce99b64c7c74a0d7452ec46059cf6
Jacek Pisarski <yyacekk@gmail.com>
2019-02-05 20:46:11 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+var currentTab = document.getElementsByClassName("tab");
+	for(var i=0; i<currentTab.length; i++)
+	{
+		currentTab[i].addEventListener("click",showTab);
+		
+	}
+
+
+
+function showTab(){
+	
+
+	var temp = document.getElementById(this.dataset.target);
+	for(var i=0; i<currentTab.length; i++)
+	{
+		var target=document.getElementById(currentTab[i].dataset.target);
+		if(target.classList.contains("show") && target!=temp)
+		{
+			target.classList.remove("show");
+		}
+	}
+	temp.classList.add("show");
+
+	
+	
+}
